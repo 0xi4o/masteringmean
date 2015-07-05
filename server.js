189cb4a3239571dd68c4bdb5781358880c2757b9
Ilango Rajagopal <rajagopalilango@gmail.com>
2015-07-05 12:45:46 +0530

    Build server with connect module

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
-var http = require('http');
+// var http = require('http');
+//
+// http.createServer(function(req,res) {
+//     res.writeHead(200,{
+//         'Content-Type': 'text/plain'
+//     });
+//
+//     res.end('Hello Node World');
+// }).listen(3000);
+//
+// console.log('Server listening at 127.0.0.1 port 3000');
 
-http.createServer(function(req,res) {
-    res.writeHead(200,{
-        'Content-Type': 'text/plain'
-    });
+var connect = require('connect');
+var app = connect();
 
+var logger = function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+};
+
+var helloNode = function(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Node World');
-}).listen(3000);
+};
+
+var goodBye = function(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Good Bye');
+};
+
+app.use(logger);
+app.use('/hello', helloNode);
+app.use('/goodbye', goodBye);
+
+app.listen(3000);
 
 console.log('Server listening at 127.0.0.1 port 3000');
